cbcd7979c4d8e9094bf7c9ad64be708091e12017
mecies <maciekhnat@gmail.com>
2020-03-29 14:13:21 +0200

    Render variable, not hardcoded stocks

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,13 +1,23 @@
 <template>
 <div class="col-sm-12 col-md-12">
   <div class="card">
-    <h3 class="card-header alert-success">NAME <small>(Price: PRICE)</small></h3>
+    <h3 class="card-header alert-success">
+      {{ stock.name }} <small>(Price: {{ stock.price }})</small>
+    </h3>
     <div class="card-body">
       <div class="float-left">
-        <input type="number" class="form-control" placeholder="Quantity" />
+        <input v-model.number="quantity"
+          type="number" class="form-control"
+          placeholder="Quantity"
+        />
       </div>
       <div class="float-right">
-        <button class="btn btn-success">Buy</button>
+        <button
+          @click="buyStock" class="btn btn-success"
+          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+        >
+          Buy
+        </button>
       </div>
     </div>
   </div>
@@ -17,6 +27,22 @@
 <script>
 export default {
   props: ['stock'],
+  data() {
+    return {
+      quantity: 0,
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity,
+      };
+      console.log(order);
+      this.quantity = 0;
+    },
+  },
 };
 </script>
 
